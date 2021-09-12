ea6c730413d9d043cdb98bd66b4fac0c3e767592
lupiic <113922521+lupiic@users.noreply.github.com>
2021-09-12 16:42:28 +0000

    all files added

--- src/core/heap.rs
+++ src/core/heap.rs
@@ -1,1 +1,10 @@
-// this lib migrate from official lib,
+// this lib migrate from official lib, but without std dependency;
+
+use core::mem::{swap, ManuallyDrop};
+use core::ptr;
+
+pub struct BinaryHeap<T> {
+    data: Vec<T>,
+}
+
+impl<T: Ord> BinaryHeap<T
