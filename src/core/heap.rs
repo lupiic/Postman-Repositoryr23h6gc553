3087e6242da4e55aee71373681ee162517acc889
lupiic <113922521+lupiic@users.noreply.github.com>
2021-09-13 16:26:09 +0000

    v0.0.16

--- src/core/heap.rs
+++ src/core/heap.rs
@@ -22,4 +22,12 @@ impl<T: Ord> BinaryHeap<T> {
         self.data.pop().map(|mut item| {
             if !self.is_empty() {
                 swap(&mut item, &mut self.data[0]);
-                self.sift_down_to_bottom(0)
+                self.sift_down_to_bottom(0);
+            }
+            item
+        })
+    }
+
+    pub fn push(&mut self, item: T) {
+        let old_len = self.len();
+        self.data.
