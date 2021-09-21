5ab4465543b409ecf5c503267f5292bb44148aa3
lupiic <113922521+lupiic@users.noreply.github.com>
2021-09-21 19:52:17 +0000

    Update index.wiki

--- src/core/heap.rs
+++ src/core/heap.rs
@@ -172,4 +172,12 @@ impl<T> BinaryHeap<T> {
 /// Hole represents a hole in a slice i.e., an index without valid value
 /// (because it was moved from or duplicated).
 /// In drop, `Hole` will restore the slice by filling the hole
-/// position with the val
+/// position with the value that was originally removed.
+struct Hole<'a, T: 'a> {
+    data: &'a mut [T],
+    elt: ManuallyDrop<T>,
+    pos: usize,
+}
+
+impl<'a, T> Hole<'a, T> {
+    /// Create a 
