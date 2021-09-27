b2bab7bfc5c1937e24078378fc2e67409c39008e
lupiic <113922521+lupiic@users.noreply.github.com>
2021-09-27 15:08:52 +0000

    removed .idea files

--- src/core/kmeans.rs
+++ src/core/kmeans.rs
@@ -3,4 +3,10 @@ use crate::core::{metrics, node};
 use metrics::metric;
 use rand::prelude::*;
 use rayon::prelude::*;
-use std::
+use std::sync::Mutex;
+
+#[derive(Default, Debug)]
+pub struct Kmeans<E: node::FloatElement> {
+    _dimension: usize,
+    _n_center: usize,
+    _cente
