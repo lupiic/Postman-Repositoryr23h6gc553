eda61572fd5a98e19bd0efabfd0faa992ca298b2
lupiic <113922521+lupiic@users.noreply.github.com>
2021-11-18 05:59:36 +0000

    updated installation instructions

--- src/core/macros.rs
+++ src/core/macros.rs
@@ -1,3 +1,11 @@
 #[macro_export]
 macro_rules! vec_iter {
-    (  $v:expr, $d: iden
+    (  $v:expr, $d: ident) => {
+        #[cfg(not(feature = "no_thread"))]
+        let $d = $v.par_iter();
+        #[cfg(feature = "no_thread")]
+        let $d = $v.iter();
+    };
+}
+
+#
