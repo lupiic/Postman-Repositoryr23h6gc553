73bcb0063d9baf57440d0644b060db62e960fc94
lupiic <113922521+lupiic@users.noreply.github.com>
2021-11-22 10:55:38 +0000

    Container

--- src/core/neighbor.rs
+++ src/core/neighbor.rs
@@ -1,3 +1,9 @@
 extern crate num;
 use crate::core::node;
-use core::
+use core::cmp::Ordering;
+
+/// internal temp structure
+///
+#[derive(Default, Clone, PartialEq, Debug)]
+pub struct Neighbor<E: node::FloatElement, T: node::IdxType> {
+    pub 
