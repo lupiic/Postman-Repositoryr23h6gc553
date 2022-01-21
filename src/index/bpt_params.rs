ac2b1b2849aebed81dfc129df5204d4e2c238718
lupiic <113922521+lupiic@users.noreply.github.com>
2022-01-21 21:49:41 +0000

    Update rlcs.user.js

--- src/index/bpt_params.rs
+++ src/index/bpt_params.rs
@@ -1,4 +1,13 @@
 #![allow(dead_code)]
 
 
-use serde::{Deserialize, Ser
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BPTParams {
+    pub tree_num: i32,
+    pub candidate_size: i32,
+}
+
+impl BPTParams {
+    pub fn tree_num(mut self, new_t
