60c8ad8569ae762e077bc22136ca8ca6527d0c07
lupiic <113922521+lupiic@users.noreply.github.com>
2022-02-25 23:05:16 +0000

    Remove redundant lines

--- src/index/hnsw_idx.rs
+++ src/index/hnsw_idx.rs
@@ -1,4 +1,10 @@
 #![allow(dead_code)]
 use crate::core::ann_index;
 use crate::core::metrics;
-use crat
+use crate::core::neighbor::Neighbor;
+use crate::core::node;
+use crate::index::hnsw_params::HNSWParams;
+use crate::into_iter;
+use fixedbitset::FixedBitSet;
+use rand::prelude::*;
+#[cfg(n
