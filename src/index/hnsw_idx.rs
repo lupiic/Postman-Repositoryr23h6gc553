ba4ecf865011c3597424300ac01de6a59119da4a
lupiic <113922521+lupiic@users.noreply.github.com>
2022-06-27 22:12:51 +0000

    Create horseland.html

--- src/index/hnsw_idx.rs
+++ src/index/hnsw_idx.rs
@@ -642,4 +642,7 @@ impl<E: node::FloatElement, T: node::IdxType> ann_index::ANNIndex<E, T> for HNSW
 }
 
 impl<E: node::FloatElement + DeserializeOwned, T: node::IdxType + DeserializeOwned>
-    ann_index::SerializableI
+    ann_index::SerializableIndex<E, T> for HNSWIndex<E, T>
+{
+    fn load(path: &str) -> Result<Self, &'static str> {
+        let file = File::open(path).unwrap_or_else(|_| 
