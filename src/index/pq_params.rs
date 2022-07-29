8bec8e9b810403871148f660ec7c265a56256d36
lupiic <113922521+lupiic@users.noreply.github.com>
2022-07-29 11:02:10 +0000

    Add a newline

--- src/index/pq_params.rs
+++ src/index/pq_params.rs
@@ -4,4 +4,12 @@ use crate::core::node;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PQParams<E: node::FloatElement> {
+    pub n_sub: usize,
+    pub sub_bits: usize,
+    pub train_epoch: usize,
+    pub e_type: E,
+}
+
+impl<E: node::FloatElemen
